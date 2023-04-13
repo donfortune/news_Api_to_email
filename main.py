4b743c0b19aa5127da3645142539019ae68b3ea2
donfortune <donfortunet.df@gmail.com>
2023-04-13 09:16:29 +0100

    major upgrades

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import requests
 from send_mail import send_email
-
+topic = 'tesla'
 api_key = 'caaad31c3aae4af7a92c2df85bfa5d78'
-url = 'https://newsapi.org/v2/everything?q=apple&from=2023-04-12&to=2023-04-12&sortBy=popularity&apiKey=caaad31c3aae4af7a92c2df85bfa5d78'
+url = "https://newsapi.org/v2/everything?" \
+      "q=tesla&" \
+      "from=2023-03-13&" \
+      "sortBy=publishedAt&" \
+      "apiKey=caaad31c3aae4af7a92c2df85bfa5d78&language=en"
 
 response = requests.get(url)
 content = response.json()
 
 body = ''
-for article in content['articles']:
-    body = body + article['title'] + '\n' + article['description'] + 2*'\n'
+
+for article in content['articles'][:20]:
+    if article["description"] is not None:
+        body = "subject: Today's News" + '\n' + body + article['title'] + '\n' + article['description'] + '\n' + article['url'] + 2*'\n'
 body = body.encode('utf-8')
 send_email(message=body)
