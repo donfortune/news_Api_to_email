b43d0953e8c1e7aa3a2a84704ae7da942c64d227
donfortune <donfortunet.df@gmail.com>
2023-04-13 08:31:51 +0100

    configured emails

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
+from send_mail import send_email
+
 api_key = 'caaad31c3aae4af7a92c2df85bfa5d78'
-url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-03-12&sortBy=publishedAt&apiKey=caaad31c3aae4af7a92c2df85bfa5d78'
+url = 'https://newsapi.org/v2/everything?q=apple&from=2023-04-12&to=2023-04-12&sortBy=popularity&apiKey=caaad31c3aae4af7a92c2df85bfa5d78'
 
-request = requests.get(url)
-content = request.json()
+response = requests.get(url)
+content = response.json()
 
+body = ''
 for article in content['articles']:
-    print(article['title'])
-
+    body = body + article['title'] + '\n' + article['description'] + 2*'\n'
+body = body.encode('utf-8')
+send_email(message=body)
---
+++ send_mail.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = 'donfortunet.df@gmail.com'
+    password = 'kktoyewnkmctutke'
+    receiver_email = 'osowoayiobi@gmail.com'
+
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as file:
+        file.login(username, password)
+        file.sendmail(username, receiver_email, message)
+
+
+
